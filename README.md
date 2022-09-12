72ca99163bf020d424e7715967a60ad0b3c2b14a
bahaa1985 <bahaa_salaheldin@live.com>
2022-09-12 22:05:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Parallax GSAP Scroll Trigger
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Bahaa1985/pen/YzwEOZr](https://codepen.io/Bahaa1985/pen/YzwEOZr).
+
---
+++ dist/script.js
@@ -0,0 +1,19 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const containers = document.querySelectorAll(".div-container");
+
+const text_elem = document.querySelectorAll(".section-header");
+
+for (let i = 0; i < text_elem.length; i++) {
+  gsap.to(text_elem[i], {
+    scrollTrigger: {
+      trigger: containers[i],
+      start: "top 90%",
+      toggleActions: "play reset play reset"
+    },
+    filter: "blur(0)",
+    duration: 0.6,
+    display: "block",
+    yoyo: "true"
+  });
+}
